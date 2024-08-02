05b2e690b8f5b275322f21f9b231e8b426895a03
Ameya-bit <54782629+Ameya-bit@users.noreply.github.com>
2024-08-02 17:25:38 +0000

    idk

--- member.js
+++ member.js
@@ -1,9 +1,4 @@
 function skillmembers() {
-    let members = [
-        {name: 'John', age: 25, skills: ['HTML', 'CSS', 'JS']},
-        {name: 'Jane', age: 22, skills: ['React', 'Node', 'JS']}
-    ];
-    members.map(member => {
-        console.log(member.skills);
-    });
+    var member = ["sandeep", "sachin", "vishal"];
+    return member;
 }
